f9ed4bca1b333aa7674bf69cc9ee48a827bbc607
VectorRepo <115223889+VectorRepo@users.noreply.github.com>
2023-06-02 16:00:15 +0200

    config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Adam',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -85,7 +85,7 @@ const CONFIG = {
 			id: '6',
 			name: 'Odysee',
 			icon: 'youtube',
-			link: 'https://odysee.com/',
+			link: 'https://youtube.com/',
 		},
 	],
 
